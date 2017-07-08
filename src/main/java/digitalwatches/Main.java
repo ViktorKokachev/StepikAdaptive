4b49581019c1b1dac1252bc96a8de206df19ede8
ViktorKokachev <kpkshke@gmail.com>
2017-07-08 19:56:48 +0300

    init commit

---
+++ src/main/java/digitalwatches/Main.java
@@ -0,0 +1,16 @@
+package digitalwatches;
+
+/**
+ *
+ Digital watches display time in the h:mm:ss format (from 0:00:00 to 23:59:59),
+    i.e. first goes the number of hours, then goes the two-digit number of minutes,
+    followed by the two-digit number of seconds.
+    If necessary, number of minutes and seconds are filled by zeroes to a two-digit number.
+
+ N seconds passed from the beginning of the day. Output what the watches will display.
+
+ Input data format:
+ Given the natural number N on input, not exceeding 10^7 (10000000).
+ */
+public class Main {
+}
